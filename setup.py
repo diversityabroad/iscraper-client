622f023e0fe2539926d30b19ee1fe9b5cbc4fed8
Joe Jasinski <jjasinski@imagescape.com>
2014-09-10 16:02:55 +0000

    update packaging info

--- setup.py
+++ setup.py
@@ -9,20 +9,12 @@ for line in f:
       if not (re.match("^[\s]*$", li) or re.match("^#.*", li)):
           dependencies.append(li)
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
     name='django-smartsearch',
     version='0.1.0',
     author='Joe Jasinski',
     author_email='jjasinski@imagescape.com',
-    url='',
+    url='git@git.imagescape.com:smartsearch2',
     description='Search frontend tool.', 
-    install_requires=[
-       'django >= 1.6',
-       'site_config >= 0.0.6',
-    ],
-    long_description=read('README.md'),
     install_requires=dependencies,
 )
