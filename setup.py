13809115c0be135cbf494e301e6cccedb78fd7a2
petercat12 <pwensel@gmail.com>
2017-11-17 13:23:54 -0600

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 NAME = 'iscraper_client'
-VERSION = '4.0.18'
+VERSION = '4.0.19'
 AUTHOR = 'Imaginary Landscape'
 DESCRIPTION = 'Client application for interacting with iscraper.imagescape.com'
 LONG_DESCRIPTION = ""
